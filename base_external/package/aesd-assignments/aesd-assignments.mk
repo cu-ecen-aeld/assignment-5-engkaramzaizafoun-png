caff82d9e05d11f48f2c0bb5d43344d3ae4912e1
engkaramzaizafoun-png <eng.karamzaizafoun@gmail.com>
2026-03-20 12:15:21 +0300

    Assignment 5: socket server build root

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -4,13 +4,15 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = 42cb2bfe82a8e181e2e0ba3f83b69e386402a7ff
+AESD_ASSIGNMENTS_VERSION = 1c67bec1d144272937e08658cd373e44c4aa8655
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-engkaramzaizafoun-png.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	
 endef
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
@@ -22,6 +24,12 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/writer
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/finder.sh
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/finder-test.sh
+	
+	
+
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+
 endef
 
 $(eval $(generic-package))
